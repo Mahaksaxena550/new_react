38a1cbf94808d71f248b47c33c7b7b8f2febe101
Mahak Saxena <ms2722528@gmail.com>
2026-01-25 09:34:58 +0530

    updated

--- src/pages/show_data.jsx
+++ src/pages/show_data.jsx
@@ -1,20 +1,70 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const show_data = () => {
     
-    useEffect(()=>{
+    let [data,setdata]=useState([])
+
+ let fatchData=()=>{
         let api='http://localhost:3000/Hotel'
 
         axios.get(api).then((res)=>{
             console.log(res.data);
+            setdata(res.data)
+
         }).catch((err)=>{
+
           console.log(err);
         })
+ }
+
+    useEffect(()=>{
+  fatchData()
     },[])
+
+
+
+  let deletedata=(id)=>{
+   let api=`http://localhost:3000/Hotel/${id}`
+
+   axios.delete(api).then(()=>{
+   
+    alert("Data Delete")
+     fatchData()
+     
+  })
+  }
+
+
   return (
     <>
+    <table>
+  
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>City</th>
+            <th>Person</th>
+            <th>Aadhar</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+   
+ 
+   <tbody>
+    {data.map((e)=>(
       
+
+      <tr key={e.id}>
+        <td>{e.name}</td>
+        <td>{e.city}</td>
+        <td>{e.person}</td>
+        <td>{e.aadhar}</td>
+        <td onClick={()=>{deletedata(e.id)}}>Delete</td>
+      </tr>
+    ))}
+   </tbody>
+     </table>
     </>
   )
 }
